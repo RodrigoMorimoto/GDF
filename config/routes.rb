430a0068c1c9ea10687fbcbfa5c44b271f559762
Thalles Batista <thalles.taeb@gmail.com>
2018-12-04 19:47:36 -0200

    links para loggin e novo usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'loggin' => 'loggins#new'
-  post 'loggin' => 'loggins#create'
-
-  root 'welcome#index'
   
+  root 'welcome#index'
   resources :systems
   resources :users
+  get 'loggin' => 'loggins#new'
+  post 'loggin' => 'loggins#create'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
